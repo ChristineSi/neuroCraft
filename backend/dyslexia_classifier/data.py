60cecf467fd23215c5be68b36c680ac9e4e87efa
Patricia Moreno Gaona <pmgaona10@gmail.com>
2023-12-08 14:50:53 +0100

    Updated classifier-data.py

--- backend/dyslexia_classifier/data.py
+++ backend/dyslexia_classifier/data.py
@@ -2,70 +2,63 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from feature_engineering import FeatureEngineer
+from text_preprocessor import TextPreprocessor
 
-def preprocess_text(text):
-    """
-    Preprocesses the input text for dyslexia classification and text simplification.
+class DyslexiaData:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.feature_engineer = FeatureEngineer()
+        self.text_preprocessor = TextPreprocessor()
+        self.data = self.load_data()
+        self.preprocess_data()
 
-    Args:
-    - text (str): Input text.
+    def load_data(self):
+        """
+        Load and preprocess the data from a CSV file.
+        """
+        df = pd.read_csv(self.file_path)
+        return df
 
-    Returns:
-    - str: Preprocessed text.
-    """
-    # Add your specific text preprocessing steps here
-    return text
+    def preprocess_data(self):
+        """
+        Preprocesses the input text for dyslexia classification and text simplification.
+        """
+        drop_columns = ['Last Changed', 'URL', 'Anthology', 'MPAA \n#Max', 'Pub Year', 'MPAA\n#Avg', 'License', 'British Words', 'firstPlace_pred', 'secondPlace_pred', 'thirdPlace_pred',
+                        'fourthPlace_pred', 'fifthPlace_pred', 'sixthPlace_pred', 'ID', 'Author', 'Title', 'Source', 'Category', 'Location', 'MPAA\nMax', 'BT s.e.', 'Kaggle split']
+        self.data = self.data.drop(columns=drop_columns)
+        self.data['excerpt'] = self.data['excerpt'].apply(self.preprocess_text)
+        label_encoder = LabelEncoder()
+        self.data['label'] = label_encoder.fit_transform(self.data['label'])
 
-def load_data(file_path):
-    """
-    Load and preprocess text data for dyslexia classification.
+    def preprocess_text(self, text):
+        # Preprocess the text using the methods from the TextPreprocessor class
+        text = self.text_preprocessor.preprocess(text)
 
-    Args:
-    - file_path (str): Path to the CSV file containing text data.
+        # Instantiate the FeatureEngineer class
+        feature_engineer = FeatureEngineer()
 
-    Returns:
-    - pd.DataFrame: Processed DataFrame with excerpts and labels.
-    """
-    # Assuming your CSV has columns 'excerpt' and 'label' for excerpts and corresponding labels
-    df = pd.read_csv(file_path)
+        # Call the methods from the FeatureEngineer class on the text
+        avg_word_length = feature_engineer.avg_word_length(text)
+        avg_syllaba_word = feature_engineer.avg_syllaba_word(text)
+        vowel_count = feature_engineer.count_vowels(text)
+        punctuation_count = feature_engineer.count_punctuation(text)
+        capital_char_ratio = feature_engineer.ratio_capital_chars(text)
+        capital_word_count = feature_engineer.count_capital_words(text)
+        word_length_std = feature_engineer.word_length_std(text)
+        sentence_len = feature_engineer.sentence_len(text)
 
-    # Preprocess the excerpts (you can customize this based on your specific requirements)
-    df['excerpt'] = df['excerpt'].apply(preprocess_text)
+        # Combine the results into a dictionary
+        features = {
+            'avg_word_length': avg_word_length,
+            'avg_syllaba_word': avg_syllaba_word,
+            'vowel_count': vowel_count,
+            'punctuation_count': punctuation_count,
+            'capital_char_ratio': capital_char_ratio,
+            'capital_word_count': capital_word_count,
+            'word_length_std': word_length_std,
+            'sentence_len': sentence_len
+        }
 
-    # Encode labels if needed (e.g., converting 'easy', 'medium', 'hard' to numerical values)
-    label_encoder = LabelEncoder()
-    df['label'] = label_encoder.fit_transform(df['label'])
-
-    return df
-
-def split_data(df, test_size=0.2, random_state=42):
-    """
-    Split the data into training and testing sets.
-
-    Args:
-    - df (pd.DataFrame): Processed DataFrame with excerpts and labels.
-    - test_size (float): Proportion of the dataset to include in the test split.
-    - random_state (int): Seed for random number generation.
-
-    Returns:
-    - pd.DataFrame: Training data.
-    - pd.DataFrame: Testing data.
-    """
-    train_df, test_df = train_test_split(df, test_size=test_size, random_state=random_state)
-    return train_df, test_df
-
-def chunk_text(excerpt, max_chunk_size=200):
-    """
-    Split a long excerpt into chunks of a specified size.
-
-    Args:
-    - excerpt (str): Input excerpt.
-    - max_chunk_size (int): Maximum number of words in each chunk.
-
-    Returns:
-    - list: List of excerpt chunks.
-    """
-    # Implement your logic to split the excerpt into chunks, considering word boundaries
-    # You may use libraries like NLTK for tokenization
-    # Return a list of excerpt chunks
-    pass
+        # Return the features
+        return features
