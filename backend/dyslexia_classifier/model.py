4594595038b53ee6d123fb0beb3a26272263f09c
Patricia Moreno Gaona <pmgaona10@gmail.com>
2023-12-08 15:09:31 +0100

    Rm old classification model folder

--- backend/dyslexia_classifier/model.py
+++
@@ -1,8 +0,0 @@
-# Model implementation for dyslexia classification
-# You can implement your dyslexia classification model here
-# Example: Dummy model for illustration purposes
-class DyslexiaClassifier:
-    def classify_text(self, text):
-        # Implement your classification logic here
-        # Return the classification result (easy, medium, hard)(0, 1, 2)
-        pass
