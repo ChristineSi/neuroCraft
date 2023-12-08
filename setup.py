80ff177ffb26450cc5ecf46765ad4b29bf894d43
Patricia Moreno Gaona <pmgaona10@gmail.com>
2023-12-08 18:37:06 +0100

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='neurocraft',
+      description="NeuroCraft Model (api_pred)",
+      #url="https://github.com/ChristineSi/neuroCraft",
+      install_requires=requirements,
+      packages=find_packages())
