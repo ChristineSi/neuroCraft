f2c888f4eb397819679e9c38d50308ade2ddeaf5
Patricia Moreno Gaona <pmgaona10@gmail.com>
2023-12-11 20:38:27 +0100

    neurocraft/backend/dyslexia_classifier/mock_model.py

--- neurocraft/backend/dyslexia_classifier/mock_model.py
+++ neurocraft/backend/dyslexia_classifier/mock_model.py
@@ -1,8 +1,24 @@
-class MockModel:
-    def predict(self, text):
-        # This is a placeholder method; replace it with your actual prediction logic
-        # For simplicity, let's assume it always predicts 'easy' for demonstration purposes
-        return 'hard'
+import fitz  # PyMuPDF
 
-# Instantiate the mock model
-mock_model = MockModel()
+def extract_text_from_pdf(pdf_path):
+    with fitz.open(pdf_path) as pdf_document:
+        num_pages = pdf_document.page_count
+
+        text = ""
+        for page_number in range(num_pages):
+            page = pdf_document[page_number]
+            text += page.get_text()
+
+    return text
+
+def chunk_text(text, chunk_size=200):
+    # Split the text into chunks of the specified size
+    chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+    return chunks
+
+# Example usage
+pdf_path = 'raw_data/The future of philosophy _ John R. Searle-1.pdf'
+pdf_text = extract_text_from_pdf(pdf_path)
+text_chunks = chunk_text(pdf_text)
+
+print(text_chunks)
