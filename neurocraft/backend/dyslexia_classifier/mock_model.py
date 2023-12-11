73bc8df113ae741ccef7d5757038417438c2c623
Patricia Moreno Gaona <pmgaona10@gmail.com>
2023-12-11 18:57:10 +0100

    Mock model

---
+++ neurocraft/backend/dyslexia_classifier/mock_model.py
@@ -0,0 +1,8 @@
+class MockModel:
+    def predict(self, text):
+        # This is a placeholder method; replace it with your actual prediction logic
+        # For simplicity, let's assume it always predicts 'easy' for demonstration purposes
+        return 'hard'
+
+# Instantiate the mock model
+mock_model = MockModel()
